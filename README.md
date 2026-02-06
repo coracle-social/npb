50b42b478462ff34c6a14b7594396a07fadc593c
Jon Staab <shtaab@gmail.com>
2026-02-06 12:59:11 -0800

    Add dockerfile

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Nostr Push Bridge
 
 This is a minimal nostr relay implementation which accepts `kind 30390` [push subscription events](https://github.com/nostr-protocol/nips/pull/2194) on behalf of public relays.
+
+## Configuration
+
+The following env variables are required:
+
+- `PORT` - the port to run the server on
+- `SECRET` - a hex secret key for the relay's identity
+
+The following env variables are optional:
+
+- `DATA_DIR` - a directory where the sqlite database should be stored (defaults to `.`)
+- `CORS_DOMAIN` - restrict domains able to connect (defaults to `*`)
--- src/env.ts
+++ src/env.ts
@@ -3,13 +3,11 @@ import { Nip01Signer } from "@welshman/signer";
 
 if (!process.env.PORT) throw new Error("PORT is not defined.");
 if (!process.env.SECRET) throw new Error("SECRET is not defined.");
-if (!process.env.DATA_DIR) throw new Error("DATA_DIR is not defined.");
-if (!process.env.CORS_DOMAIN) throw new Error("CORS_DOMAIN is not defined.");
 
 export const PORT = process.env.PORT;
 export const signer = Nip01Signer.fromSecret(process.env.SECRET);
-export const DATA_DIR = process.env.DATA_DIR;
-export const CORS_DOMAIN = process.env.CORS_DOMAIN;
+export const DATA_DIR = process.env.DATA_DIR || ".";
+export const CORS_DOMAIN = process.env.CORS_DOMAIN || "*";
 
 signer.getPubkey().then((pubkey) => {
   console.log(`Running as ${pubkey}`);
